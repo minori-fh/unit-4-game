b9c5dba5e91519561a256717316e8ab57bdd4957
Minori Hashimoto <minori.fh@gmail.com>
2019-05-29 21:29:49 -0700

    js: add initially defined global variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//Document ready
+$(document).ready(function() {
+    
+//Defined variables
+var yourCharacter = "";
+var yourEnemies = "";
+var yourOpponent = ""; 
+
+var characterChosen = false;
+var opponentChosen = false;
+var win = false;
+var firstRound = true; 
+var winCount = 0;
+
+var attackCount = 0; 
+var originalState = $("#character-pick").clone();
+     
+}); //END document ready end
