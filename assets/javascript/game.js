e1533545433fee96bf5f3a3ad548984513e06dbe
Minori Hashimoto <minori.fh@gmail.com>
2019-05-29 21:33:16 -0700

    js: add functionality to pick characters and place in appropriate areas of the DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,5 +14,18 @@ var winCount = 0;
 
 var attackCount = 0; 
 var originalState = $("#character-pick").clone();
-     
+
+//On click functionaliy to pick character
+$(document).on("click", ".character", function() { //putting event listener on document vs. specific element
+if (characterChosen === false){
+
+    yourCharacter = $(this).html(); //grab the html contents of character that was clicked on
+    $(this).remove();
+
+    $("#your-character").append(yourCharacter); //place the html contents from ^ into appropriate "your-character" div
+    $("#your-character-02").append(yourCharacter);
+    $("#your-character-02").css("color","white");
+}
+})
+
 }); //END document ready end
